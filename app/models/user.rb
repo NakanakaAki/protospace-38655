26373cc64445c568c24c13d3b3fbd696eb675ed4
NakanakaAki <akitonakahara0918@gmail.com>
2023-01-17 22:14:36 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :name, presence: true
+  validates :password, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :postion, presence: true
 
   has_many :prototypes
   has_many :comments
