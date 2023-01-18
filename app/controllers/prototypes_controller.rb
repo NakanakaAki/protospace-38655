d8d8cc0c59755fd0e8243bcfc9464aff2f1b22ac
NakanakaAki <akitonakahara0918@gmail.com>
2023-01-18 22:39:11 +0900

    Update

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy ]
-
+  before_action :move_index, only: [:edit ]
 
 
   def index
@@ -29,9 +29,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in? == @Prototype
-      redirect_to root_path
-    end
   end
 
   def update
@@ -54,8 +51,15 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :content, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_index
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end 
 
 end
 
